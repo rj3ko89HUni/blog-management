3c9819b7c860355a9f3298f32ab23019e93dc9fd
Chiba Takaki <takaki200109@gmail.com>
2022-03-31 15:52:12 +0900

    c1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-    }
+      if (\App::environment('production')) {
+        　\URL::forceScheme('https');
+      }
 }
